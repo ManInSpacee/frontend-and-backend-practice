db2e29bfcdd89d3d57cf7e7da502d43492bbc4cf
ManInSpace <klimentmart@gmail.com>
2025-10-09 19:13:24 +0300

    Add modal form

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-console.log('Happy developing ✨')
+const dialog = document.getElementById('MyDialog');
+const openBtn = document.getElementById('OpenBtn');
+const closeBtn = document.getElementById('CloseBtn');
+const submitBtn = document.getElementById('SubmitBtn');
+const form = document.getElementById('feedbackForm');
+
+
+openBtn.addEventListener('click', () => {
+  dialog.showModal();
+});
+
+closeBtn.addEventListener('click', () => {
+  dialog.close();
+})
+
+
+// Закрытие по клику на фон
+dialog.addEventListener('click', (event) => {
+  const rect = dialog.getBoundingClientRect();
+  const clickedOutside =
+    event.clientX < rect.left ||
+    event.clientX > rect.right ||
+    event.clientY < rect.top ||
+    event.clientY > rect.bottom;
+
+  if (clickedOutside) dialog.close();
+});
+
+// Отправка формы
+submitBtn.addEventListener('click', () => {
+  if (!form.checkValidity()) {
+    form.reportValidity(); // встроенная браузерная проверка
+    return;
+  }
+
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData.entries());
+
+  console.log('Форма отправлена:', data);
+  alert('Спасибо, форма успешно отправлена!');
+
+  form.reset();
+});
